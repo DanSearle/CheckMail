4964fd984e3e1358dad9155419179eea36413696
Daniel Searle <dan@d-searle.co.uk>
2015-05-03 13:40:14 +0100

    Added log messages where the verbosity can be increased using multiple -v switches and added option to automatically clean the mbox file in place

--- checkmail.py
+++ checkmail.py
@@ -8,44 +8,59 @@
 
 import pyclamd
 import argparse
-import sys
 import mailbox
+import logging
+
+log = logging.getLogger(__name__)
+default_net = ("localhost", 3310)
 
 
-def print_message(parsed, signature=None):
-    print "From: {0}, Subject: {1}, Signature: {2}".format(parsed["From"],
-                                                           parsed["Subject"],
-                                                           signature)
+class ScanMessage:
+    def __init__(self, key, message):
+        self._message = message
+        self.key = key
+        self.signature = None
+        self._scan()
 
-def scan_mail(message):
-    result = pyclamd.scan_stream(message.as_string())
-    if not result:
-        return
+    def _scan(self):
+        log.debug("Scanning message {0}".format(self))
+        result = pyclamd.scan_stream(self._message.as_string())
+        log.debug("Scanned message {0}, result {1}".format(self, result))
+        if result:
+            self.signature = result["stream"]
 
-    print_message(message, result["stream"])
+    def __repr__(self):
+        message = self._message
+        return "From: {0}, Subject: {1}, Signature: {2}".format(message["From"],
+                                                                message["Subject"],
+                                                                self.signature)
 
 
-class HostPortAction(argparse.Action):
-    def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        if nargs is not None:
-            raise ValueError("nargs not allowed")
-        super(HostPortAction, self).__init__(option_strings, dest, **kwargs)
+def parse_command_line():
+    class HostPortAction(argparse.Action):
+        def __init__(self, option_strings, dest, nargs=None, **kwargs):
+            if nargs is not None:
+                raise ValueError("nargs not allowed")
+            super(HostPortAction, self).__init__(option_strings, dest, **kwargs)
 
-    def __call__(self, parser, namespace, values, option_string=None):
-        values = values.split(":")
-        if len(values) == 1:
-            values.append(3310)
-        else:
-            values[1] = int(values[1])
-        setattr(namespace, self.dest, tuple(values))
-
-if __name__ == "__main__":
-    default_net = ("localhost", 3310)
+        def __call__(self, parser, namespace, values, option_string=None):
+            values = values.split(":")
+            if len(values) == 1:
+                values.append(3310)
+            else:
+                values[1] = int(values[1])
+            setattr(namespace, self.dest, tuple(values))
 
     parser = argparse.ArgumentParser()
     parser.add_argument('mailfile', nargs='+', type=argparse.FileType('r'),
                         help="mbox mail file to parse, must be a file and not stdin")
-    group = parser.add_argument_group('ClamConn')
+    parser.add_argument('-c', '--clean', action="store_true",
+                        help="Set to automatically remove messages which have detected viruses")
+    parser.add_argument("-v", "--verbose", dest="verbose_count",
+                        action="count", default=0,
+                        help="increases log verbosity for each occurence.")
+
+    group = parser.add_argument_group('Clamd Connection')
     group_ex = group.add_mutually_exclusive_group()
     group_ex.add_argument('-s', '--socket', metavar="SOCKET", type=str,
                           default="/var/run/clamav/clamd.ctl",
@@ -53,7 +68,12 @@ if __name__ == "__main__":
     group_ex.add_argument('-n', '--network', metavar="HOST:PORT", type=str, action=HostPortAction,
                           default=default_net,
                           help="Host and port to contact clamd, e.g. localhost:3310")
-    args = parser.parse_args()
+    arguments = parser.parse_args()
+    logging.basicConfig(level=max(2 - arguments.verbose_count, 0) * 10)
+    return arguments
+
+if __name__ == "__main__":
+    args = parse_command_line()
 
     if args.network == default_net:
         try:
@@ -64,5 +84,24 @@ if __name__ == "__main__":
         pyclamd.init_network_socket(args.network[0], args.network[1])
 
     for filename in args.mailfile:
-        for msg in mailbox.mbox(filename.name):
-            scan_mail(msg)
+        log.debug("Reading mboxfile {0}".format(filename.name))
+        mbox = mailbox.mbox(filename.name)
+        log.debug("Loaded mboxfile {0}".format(filename.name))
+        try:
+            virus_mail = [y for y in [ScanMessage(key, message) for key, message in mbox.iteritems()] if y.signature]
+            for v in virus_mail:
+                log.info("Found virus in message {0}".format(v))
+                if args.clean:
+                    log.debug("Locking mailbox {0}".format(filename.name))
+                    mbox.lock()
+                    try:
+                        log.debug("Cleaning {0} from mailbox {1}".format(v, filename.name))
+                        log.info("Message {0} removed".format(v.key))
+                        mbox.remove(v.key)
+                        log.debug("Flushing mailbox {0}".format(filename.name))
+                        mbox.flush()
+                    finally:
+                        log.debug("Unlocking mailbox {0}".format(filename.name))
+                        mbox.unlock()
+        finally:
+            mbox.close()
